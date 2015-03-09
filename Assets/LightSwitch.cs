69e89d9f349c4f96b24cf93c354437be76b06c83
Kevin WASER <kevin.waser@epita.fr>
2015-03-09 22:40:09 +0100

    [Leap] Added Leap Motion Controller

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+	public GameObject light;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnTriggerEnter() {
+		light.SetActive (!light.activeSelf);
+	}
+}
