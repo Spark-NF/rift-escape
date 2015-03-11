d37284401a723cd9e9c509c19af79bd089efe0f3
Nicolas Faure <faure_n@epita.fr>
2015-03-11 16:36:26 +0100

    Fix main menu for Oculus.

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour
+{
+	public float time = 1f;
+	public float startingAlpha = 0f;
+	public float endAlpha = 1f;
+	private CanvasGroup canvasGroup;
+
+	void Awake()
+	{
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	void Start ()
+	{
+		canvasGroup.alpha = startingAlpha;
+		StartCoroutine("Fade");
+	}
+	
+	IEnumerator Fade()
+	{
+		while (canvasGroup.alpha < endAlpha)
+		{
+			canvasGroup.alpha += Time.deltaTime / time;
+			if (canvasGroup.alpha > endAlpha)
+			{
+				canvasGroup.alpha = endAlpha;
+			}
+			yield return null;
+		}
+	}
+}
