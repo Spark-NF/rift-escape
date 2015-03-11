817771e39b075d0a411c6a296728d235a9853b00
Nicolas Faure <faure_n@epita.fr>
2015-03-11 18:52:01 +0100

    Fix menu navigation with arrows and enter keys.

---
+++ Assets/MenuEvents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEvents : MonoBehaviour
+{
+	public void MenuQuit()
+	{
+		Application.Quit();
+	}
+
+	public void MenuPlay()
+	{
+		Application.LoadLevel("Room");
+	}
+
+	public void MenuOptions()
+	{
+		Debug.LogWarning("Options");
+	}
+}
