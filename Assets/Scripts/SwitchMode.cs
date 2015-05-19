06cf758d86c97d7d3f1df4d9113f7b5108d87be3
Kevin WASER <kevin.waser@epita.fr>
2015-05-19 16:12:27 +0200

    [Leap] Started Switch Mode between Oculus and Leap

---
+++ Assets/Scripts/SwitchMode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Leap;
+
+public class SwitchMode : MonoBehaviour {
+
+	public HandController controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.N)) {
+			controller.isHeadMounted = false;
+			controller.transform.rotation = new Quaternion(350, 180, 0, 0);
+			controller.transform.position = new Vector3(0, -0.2f, 0.2f);
+		}
+	}
+}
