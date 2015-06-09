ed6d30a5c06faeca86d97b73e31ccbe697b9fdf8
Kevin WASER <kevin.waser@epita.fr>
2015-06-09 17:40:56 +0200

    [LEAP] PauseMenu Btn On Hand Tests

---
+++ Assets/Scripts/PauseMenuBtn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuBtn : MonoBehaviour {
+
+	public Canvas btn;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (btn);
+		btn.gameObject.SetActive (true);
+		btn.transform.SetParent (transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnDestroy() {
+		DestroyObject (btn);
+	}
+
+}
