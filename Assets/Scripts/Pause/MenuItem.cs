b9fd00159654bd80fd5c0a8655f7bdc683a59461
Nicolas Faure <faure_n@epita.fr>
2015-06-09 18:46:33 +0200

    Add timer to prevent multiple fires in pause menu using the Leap

--- Assets/Scripts/Pause/MenuItem.cs
+++ Assets/Scripts/Pause/MenuItem.cs
@@ -4,12 +4,18 @@ using System.Collections;
 public abstract class MenuItem : MonoBehaviour
 {
 	public KeyCode letter = 0;
+	protected float fireRate = 1.0f;
+	protected float nextFire = 0.0f;
 
 	public abstract void activate();
 
 	void OnTriggerEnter(Collider other)
 	{
-		activate();
+		if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>() && Time.time > nextFire)
+	    {
+			activate();
+			nextFire = Time.time + fireRate;
+		}
 	}
 
 	void Update()
