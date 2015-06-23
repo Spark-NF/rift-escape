2e617eeddff882e5c4b8a3fde57f04275ab1ecda
Nicolas Faure <faure_n@epita.fr>
2015-06-23 23:25:37 +0200

    Add OVR rotation setting

---
+++ Assets/Scripts/Pause/MenuValueOvrRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuValueOvrRotation : MenuValue
+{
+	public override void change(int oldValue, int newValue)
+	{
+		// FIXME change OVR rotation
+		Debug.Log("OVR change rotation");
+	}
+}
