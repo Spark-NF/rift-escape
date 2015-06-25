e24a5683df297f749b437d633b3a92c4e917ec20
Kevin WASER <kevin.waser@epita.fr>
2015-06-25 18:54:15 +0200

    [LEAP] Added Timer in Pause BTN

--- Assets/Scripts/Pause/PauseButtonHand.cs
+++ Assets/Scripts/Pause/PauseButtonHand.cs
@@ -5,6 +5,8 @@ public class PauseButtonHand : MonoBehaviour {
 
 	bool triggered = false;
 
+	int timer = -1;
+
 	private bool IsHand(Collider other)
 	{
 		if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>())
@@ -15,11 +17,19 @@ public class PauseButtonHand : MonoBehaviour {
 	
 	void OnTriggerStay(Collider other) 
 	{
-		if (IsHand (other) && !triggered) {
+		if (IsHand (other) && !triggered && timer == -1) {
 			triggered = true;
+			timer = 0;
 			PauseMenu pm = GameObject.FindGameObjectWithTag("Player").GetComponent<PauseMenu>();
 			pm.togglePause ();
 		}
+	}
+
+	public void Update() {
+		if (timer >= 0)
+			timer ++;
+		if (timer >= 100)
+			timer = -1;
 	}
 
 	void OnTriggerExit(Collider other) 
