c4b7c9f28cfe254621275e585272e2b2e5a6f1e4
Nicolas Faure <faure_n@epita.fr>
2015-07-14 15:57:50 +0200

    Add debug console

---
+++ Assets/DebugConsole.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DebugConsole : MonoBehaviour
+{
+	public GameObject canvas = null;
+	public GameObject input = null;
+	public KeyCode toggleKey = KeyCode.O;
+
+	protected bool visible = false;
+	protected InputField inputField = null;
+
+	void Start()
+	{
+		inputField = input.GetComponent<InputField>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(toggleKey) && !visible)
+		{
+			toggleConsole();
+		}
+	}
+
+	public void toggleConsole()
+	{
+		visible = !visible;
+		canvas.SetActive(visible);
+
+		if (visible)
+		{
+			EventSystem.current.SetSelectedGameObject(inputField.gameObject, null);
+			inputField.OnPointerClick(new PointerEventData(EventSystem.current));
+		}
+	}
+
+	public void DebugCommand(string msg)
+	{
+		string command = inputField.text;
+
+		switch (command)
+		{
+			case "quit":
+			case "exit":
+				Application.Quit();
+				break;
+
+			default:
+				Debug.Log("Debug command: '" + command + "'");
+				break;
+		}
+		
+		inputField.text = "";
+		toggleConsole();
+	}
+}
