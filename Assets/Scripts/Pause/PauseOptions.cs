357c5d55ae58850c057098b168daf794e47cd3d7
Nicolas Faure <faure_n@epita.fr>
2015-07-16 14:29:54 +0200

    Start fix event system for submenus

--- Assets/Scripts/Pause/PauseOptions.cs
+++ Assets/Scripts/Pause/PauseOptions.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class PauseOptions : MenuItem
 {
 	public GameObject currentPanel = null;
 	public GameObject nextPanel = null;
+	public EventSystem eventSystem = null;
 
 	public override void activate()
 	{
 		currentPanel.SetActive(false);
 		nextPanel.SetActive(true);
+
+		if (eventSystem != null)
+			eventSystem.SetSelectedGameObject(nextPanel, null);
 	}
 }
