801001189ebac0d83ab6e30d00228f7bc8de6b0f
Julien Dubois <julien.dubois@epita.fr>
2015-09-28 14:31:31 +0200

    [LIVINGROOM] Adding interactions with emergency stop button

---
+++ Assets/Scripts/LivingRoom/LaserButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserButton : MonoBehaviour {
+
+	public GameObject lasers;
+	public Vector3 offPosition;
+	public float animSpeed;
+	public AudioClip ActivateSound;
+	private bool animation = false;
+	public static bool LaserDisabled = false;
+	public AudioSource source;
+
+	// Update is called once per frame
+	void Update () {
+		if (animation) {
+			transform.localPosition = new Vector3(GetChange(offPosition.x, transform.localPosition.x),
+												  GetChange(offPosition.y, transform.localPosition.y),
+												  GetChange(offPosition.z, transform.localPosition.z));
+			animation = !(Vector3.SqrMagnitude(transform.localPosition - offPosition) < 0.0001f);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (lasers != null) {
+			lasers.SetActive(false);
+			LaserDisabled = true;
+			if (ActivateSound != null)
+				source.PlayOneShot(ActivateSound);
+			animation = true;
+			lasers = null;
+		}
+	}
+
+	private float GetChange(float destination, float current)
+	{
+		float diff = destination - current;
+		if (Mathf.Abs(diff) <= animSpeed) {
+			return diff;
+		}
+		return diff < 0 ? current - animSpeed : current + animSpeed;
+	}
+
+}
