6aabea3b88c879a78d76affc939d0c5b1648e326
Rave-D <dhennin.arnaud@gmail.com>
2015-09-28 15:43:23 +0200

    [Scenes] Add the possibility to turn the TV on and to display a BSOD on the PC.

---
+++ Assets/Scripts/ChangeTexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTexture : MonoBehaviour {
+	public bool on = false;
+	public GameObject source;
+	public Material on_texture;
+	public Material off_texture;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (on)
+		{
+			source.GetComponent<Renderer>().material = off_texture;
+			on = false;
+		}
+		else
+		{
+			source.GetComponent<Renderer>().material = on_texture;
+			on = true;
+		}
+	}
+}
