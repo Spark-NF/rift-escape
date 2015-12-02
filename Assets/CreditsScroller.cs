ab2f6c4c4a6d65d53f2c9ff05193f7b79da1e657
Nicolas Faure <faure_n@epita.fr>
2015-12-02 03:15:12 +0100

    Add credits to last room

---
+++ Assets/CreditsScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScroller : MonoBehaviour
+{
+	public GameObject content;
+	public float initScroll;
+
+	void Start ()
+	{
+		content.transform.Translate(0, -initScroll, 0);
+	}
+
+	void Update()
+	{
+		content.transform.Translate(0, Time.deltaTime / 1.0f, 0);
+
+		if (content.transform.localPosition.y > 3)
+			content.transform.Translate(0, -initScroll - 4, 0);
+	}
+}
