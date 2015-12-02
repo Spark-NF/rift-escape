b93ceaf1e307f80e8629d4dc49007891f4d0dafe
Julien Dubois <julien.dubois@epita.fr>
2015-12-02 23:12:09 +0100

    [TV] Add leap camera feedback

---
+++ Assets/Scripts/TouchEnableObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEnableObject : MonoBehaviour {
+    public bool on = false;
+    public GameObject ToDisable;
+    public AudioSource audioSource = null;
+    public AudioClip sound = null;
+
+    bool canChange = true;
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (canChange)
+        {
+            canChange = false;
+            if (on)
+            {
+                if (sound == null)
+                    audioSource.Stop();
+                on = false;
+            }
+            else
+            {
+                if (sound == null)
+                    audioSource.Play();
+                else
+                    audioSource.PlayOneShot(sound);
+                on = true;
+            }
+            ToDisable.SetActive(on);
+            yield return new WaitForSeconds(2);
+            canChange = true;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        ToDisable.SetActive(on);
+	}
+}
